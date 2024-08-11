fe359c7096e8957fd50a3519ef8fdbf228f3d562
hrishikapandey <hrishiap38@gmail.com>
2024-08-12 00:11:51 +0530

    some commit

--- Project/src/Components/Navbar.jsx
+++ Project/src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import React,{useState} from 'react';
 import styles from './Navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch,faBars } from '@fortawesome/free-solid-svg-icons';
+import logo from '../assets/logonew.png'
 
 const Navbar = () => {
   const [menuActive, setMenuActive] = useState(false);
@@ -12,7 +13,7 @@ const Navbar = () => {
   };
   return (
     <nav className={styles.navbar}>
-    <div className={styles.logo}><img src='./src/assets/logonew.png'></img></div>
+    <div className={styles.logo}><img src={logo}></img></div>
     <ul className={`${styles.navItems} ${menuActive ? styles.active : ''}`}>
         <li><a href="#home">Products</a></li>
         <li><a href="#home">Enterprise</a></li>
